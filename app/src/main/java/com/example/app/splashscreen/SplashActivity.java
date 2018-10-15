92628a9e7e76cefc22c4002ba38adeb96a9add75
Muthuraja <muthuraja.g@newgendigital.com>
2018-10-15 15:40:54 +0530

    First Commit

---
+++ app/src/main/java/com/example/app/splashscreen/SplashActivity.java
@@ -0,0 +1,36 @@
+package com.example.app.splashscreen;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.sql.Time;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        }, 3000);
+
+
+//        new Timer().schedule(new TimerTask() {
+//            @Override
+//            public void run() {
+//                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+//                finish();
+//            }
+//        }, 3000);
+
+    }
+}
